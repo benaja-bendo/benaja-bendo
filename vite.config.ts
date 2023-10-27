dac612fccf60cb0affad2e15f938d9f055d14d90
benji <64331956+benaja-bendo@users.noreply.github.com>
2023-10-27 13:26:35 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'node:path';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/benaja-bendo/",
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
