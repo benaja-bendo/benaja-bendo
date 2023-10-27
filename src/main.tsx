d897a1b1e7daeb8a5b298fd06843495b0efaeebf
benaja-bendo <benaja.bendo02@gmail.com>
2023-10-27 13:37:54 +0200

    feat: update main for test

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-    RouterProvider,
-} from "react-router-dom";
-import {Router} from "./routes";
+// import {
+//     RouterProvider,
+// } from "react-router-dom";
+// import {Router} from "./routes";
+import {Home} from "./pages/Home.tsx";
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <RouterProvider router={Router}/>
+        {/*<RouterProvider router={Router}/>*/}
+        <Home/>
     </React.StrictMode>,
 )
