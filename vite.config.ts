c86818d145c090ef9a48d9c8e0106780f0c35198
benaja-bendo <benaja.bendo02@gmail.com>
2023-10-27 10:48:49 +0200

    fix: update workflow and vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'node:path';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    // base: '/vite-deploy-demo/',
+    base: '/benaja-bendo/',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
